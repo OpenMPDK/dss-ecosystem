f7c0391676c37854ef0b366601165571dd60f809
Suresh Grandhi <suresh.g@samsung.com>
2021-03-08 13:13:45 -0800

    Move the data integrity check for GET calls as a separate function. Added some optimizations

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -13,6 +13,10 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 object_data = None
 object_data_md5 = None
+data_dir = '/tmp/'
+data_file_ref = data_dir + 'datafile_ref'
+data_md5_file_ref = data_dir + 'data_md5_file_ref'
+
 put_count = None
 put_fail_count = None
 get_count = None
@@ -20,7 +24,7 @@ get_fail_count = None
 del_count = None
 del_fail_count = None
 
-log_level = 'DEBUG'
+log_level = 'INFO'
 log_file = 'dss_benchmark.log'
 
 # Puts per thread
@@ -131,18 +135,13 @@ class DSSClient(object):
 
 
 def run_data_put(client_conn, thr_id, key_prefix, num_ios=0):
-    for i in range(num_ios):
-        filename = '/tmp/%s-object-%s-%d' % (key_prefix, thr_id, i)
-        with open(filename, 'wb') as f:
-            f.write(object_data)
-
     start_time = time.time()
     count = 0
     while True:
         if num_ios and count >= num_ios:
             break
         key = '%s-object-%s-%d' % (key_prefix, thr_id, count)
-        filename = '/tmp/' + key
+        filename = data_file_ref
         try:
             client_conn.put_object(key, filename)
             count = count + 1
@@ -151,11 +150,8 @@ def run_data_put(client_conn, thr_id, key_prefix, num_ios=0):
             put_fail_count.increment()
     end_time = time.time()
 
-    for i in range(num_ios):
-        filename = '/tmp/%s-object-%s-%d' % (key_prefix, thr_id, i)
-        os.unlink(filename)
-
-    logger.info('PUT objects - count %d - time %d', count, (end_time - start_time))
+    logger.info('PUT objects - total %d, failed %d, time %d ms', count, put_fail_count.value(),
+                (end_time - start_time) * 10**6)
     puts_per_thread[thr_id] = count
 
     '''
@@ -199,7 +195,7 @@ def run_data_get(client_conn, thr_id, key_prefix, num_ios=0):
         if num_ios and count >= num_ios:
             break
         key = '%s-object-%s-%d' % (key_prefix, thr_id, count)
-        filename = key
+        filename = data_dir + key
         count = count + 1
         try:
             client_conn.get_object(key, filename)
@@ -209,16 +205,23 @@ def run_data_get(client_conn, thr_id, key_prefix, num_ios=0):
 
     end_time = time.time()
 
+    logger.info('GET objects - count %d, failed %d,  time %d ms', count,
+                get_fail_count.value(), (end_time - start_time) * 10**6)
+
+
+def check_data_after_get(client_conn, thr_id, key_prefix, num_ios=0):
+    fail_count = FastWriteCounter()
     for i in range(num_ios):
-        filename = '%s-object-%s-%d' % (key_prefix, thr_id, i)
+        key = '%s-object-%s-%d' % (key_prefix, thr_id, i)
+        filename = data_dir + key
         with open(filename, 'rb') as file:
             output = file.read()
             output_md5 = hashlib.md5(output).hexdigest()
             logger.debug('Obj MD5 - %s, Actual MD5 - %s', output_md5, object_data_md5)
             if output_md5 != object_data_md5:
-                logger.error("Key %s didn't match with object data", filename)
-
-    logger.info('GET objects - count %d - time %d seconds', count, (end_time - start_time))
+                logger.error("Key %s didn't match with object data", key)
+                fail_count.increment()
+    logger.info('CHECK data - total %d, mismatch %d', num_ios, fail_count.value())
 
 
 def run_data_del(client_conn, thr_id, key_prefix, num_ios=0):
@@ -230,14 +233,15 @@ def run_data_del(client_conn, thr_id, key_prefix, num_ios=0):
         key = '%s-object-%s-%d' % (key_prefix, thr_id, count)
         count = count + 1
         try:
-            client_conn.get_object(key)
+            client_conn.del_object(key)
             del_count.increment()
         except:
             del_fail_count.increment()
 
     end_time = time.time()
 
-    logger.info('DEL objects - count %d - time %d', count, (end_time - start_time))
+    logger.info('DEL objects - count %d, failed %d, time %d ms', count,
+                del_fail_count.value(), (end_time - start_time) * 10**6)
 
 
 if __name__ == '__main__':
@@ -267,19 +271,20 @@ if __name__ == '__main__':
     if args.op_type in [0, 1]:
         object_data = os.urandom(args.object_size * 1024)
         object_data_md5 = hashlib.md5(object_data).hexdigest()
-        with open('data_file_ref', 'wb') as f:
+        with open(data_file_ref, 'wb') as f:
             f.write(object_data)
             logger.info('Saved the object data to datafile_ref')
-        with open('data_md5_file_ref', 'w') as f:
+        with open(data_md5_file_ref, 'w') as f:
             f.write(object_data_md5)
+            logger.info('Saved the object data md5 hash to datafile_ref')
     else:
-        if not os.stat('data_file_ref'):
+        if not os.stat(data_file_ref):
             logger.info('No data file present to check. Exiting')
             sys.exit(-1)
 
-        with open('data_file_ref', 'rb') as f:
+        with open(data_file_ref, 'rb') as f:
             object_data = f.read()
-        with open('data_md5_file_ref', 'r') as f:
+        with open(data_md5_file_ref, 'r') as f:
             object_data_md5 = f.read()
 
         if object_data_md5 != hashlib.md5(object_data).hexdigest():
@@ -293,9 +298,9 @@ if __name__ == '__main__':
     del_count = FastWriteCounter()
     del_fail_count = FastWriteCounter()
 
-    fn_list = {0: [run_data_put, run_data_get, run_data_del],
+    fn_list = {0: [run_data_put, run_data_get, check_data_after_get, run_data_del],
                1: [run_data_put],
-               2: [run_data_get],
+               2: [run_data_get, check_data_after_get],
                3: [run_data_del]
                }
 
@@ -303,12 +308,12 @@ if __name__ == '__main__':
         for fn in fn_list[args.op_type]:
             with ThreadPoolExecutor() as executor:
                 future_tasks = []
-                for thr_id in range(args.thr_cnt):
+                for i in range(args.thr_cnt):
                     client_hdl = DSSClient(args.endpoint_url, args.access_key, args.secret_key, logger)
                     if not client_hdl.client:
                         sys.exit(-1)
                     try:
-                        task = executor.submit(fn, client_hdl, thr_id, args.key_prefix,
+                        task = executor.submit(fn, client_hdl, i, args.key_prefix,
                                                args.num_ios)
                         future_tasks.append(task)
                     except Exception as e:
