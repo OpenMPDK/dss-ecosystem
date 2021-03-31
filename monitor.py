6e756b52218b9235fafb895d7b5403d4357f4736
Mahsa Bayati <mahsa.b@samsung.com>
2021-03-31 13:33:28 -0700

    update BSD copyright year

--- monitor.py
+++ monitor.py
@@ -2,7 +2,7 @@
 """
  *   BSD LICENSE
  *
- *   Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ *   Copyright (c) 2021 Samsung Electronics Co., Ltd.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
