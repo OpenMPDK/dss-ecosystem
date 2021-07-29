f4c99fd4223bce352d57f1a392d77a1741e16d86
ramtin afshar <afshar.r@jh-ramtin.msl.lab>
2021-07-29 13:52:48 -0700

    integrated

--- main_dss.py
+++ main_dss.py
@@ -8,18 +8,24 @@ import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.distributed as dist
 import torch.optim
-import torch.multiprocessing as multiprocessing
+import torch.multiprocessing as mp
 import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
+#from torch.profiler import profile, record_function, ProfilerActivity
+
+from model import *
+
 parser = argparse.ArgumentParser(description="a simple AI model on cifar100")
-parser.add_argument("--worker", "-w", type=int, default=2, help="the number of workers, default is 0")
+parser.add_argument("--worker", "-w", type=int, default=4, help="the number of workers, default is 0")
 parser.add_argument("--batch", "-b", type=int, default=16, help="batch size")
+parser.add_argument("--client", "-c", type=int, default=1, help="the number of dss client instances")
 parser.add_argument("--epochs", "-e", type=int, default=1, help="epoches of training")
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+parser.add_argument("--model","-m", type=str, default="vae", help="model name" )
+parser.add_argument('--lr', default=0.1, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
@@ -28,34 +34,52 @@ parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                     dest='weight_decay')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
+
 args = parser.parse_args()
 
 # specify S3 storage parameters
 access_key = "minio"
 secret_key = "minio123"
-endpoint = "http://202.0.0.3:9000"
+endpoint = "http://202.0.0.3:9000" #this needs to be changed
 option = dss.clientOption()
 option.maxConnections = 1
 
 # create dataloader instance
 data_loader = torch.utils.data.DataLoader
 
+"""
+normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
+                                     std=[0.2023, 0.1994, 0.2010])
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+
+                        
 transform = transforms.Compose([
-            # transforms.Resize(256),
-            # transforms.CenterCrop(224),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
         ])
 
-# create dataset instance for training and test
-train_ds = DSSIterableDataset(prefix="train", access_key=access_key, secret_key=secret_key, endpoint=endpoint, dss_option=option, transforms=transform)
-test_ds = DSSIterableDataset(prefix="train", access_key=access_key, secret_key=secret_key, endpoint=endpoint, dss_option=option)
+       """ 
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
-train_loader = data_loader(train_ds, batch_size=args.batch, num_workers=args.worker)
-test_loader = data_loader(test_ds, batch_size=args.batch, num_workers=args.worker)
+def collate_fn(batch):
+    batch = list(filter(lambda x: x is not None, batch))
+    return torch.utils.data.dataloader.default_collate(batch)
+
+# create dataset instance for training and test
+# train_ds = DSSIterableDataset(prefix="train", access_key=access_key, secret_key=secret_key, endpoint=endpoint, dss_option=option, transforms=transform)
+#test_ds = DSSDataset(prefix="map-train", access_key=access_key, secret_key=secret_key, endpoint=endpoint, dss_option=option, client_num=args.client, transforms=transform)
+train_ds = DSSDataset(prefix="cifar100-train", access_key=access_key, secret_key=secret_key, endpoint=endpoint, dss_option=option, client_num=args.client, transforms=transform_train)
+
+train_loader = data_loader(train_ds, batch_size=args.batch, num_workers=args.worker, shuffle=True, collate_fn=collate_fn)
+#test_loader = data_loader(test_ds, batch_size=args.batch, num_workers=args.worker)
+
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -103,62 +127,52 @@ def adjust_learning_rate(optimizer, epoch, args):
         param_group['lr'] = lr
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-    
-def train(train_loader, model, criterion, optimizer, epoch, args):
+def train(train_loader, model, optimizer, epoch, args):
     batch_time = AverageMeter('Time', ':6.3f')
     data_time = AverageMeter('Data', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
+    #top1 = AverageMeter('Acc@1', ':6.2f')
+    #top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(
         len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
+        [batch_time, data_time, losses],
         prefix="Epoch: [{}]".format(epoch))
     metric_df = pd.DataFrame(columns=["epoch", "iteration", "dataloading_time", "training_time", "batch_time"])
 
+    # with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
     # switch to train mode
     model.train()
 
     end = time.time()
-    for i, (images, target) in enumerate(train_loader):
+    for i, (images, _) in enumerate(train_loader):
         # measure data loading time
         dataloading_time = time.time() - end
         data_time.update(dataloading_time)
 
-        if args.gpu is not None:
-            images = images.cuda(args.gpu, non_blocking=True)
-        if torch.cuda.is_available():
-            target = target.cuda(args.gpu, non_blocking=True)
+        iamges = images.to(device)
 
-        # compute output
-        output = model(images)
-        loss = criterion(output, target)
+        #if args.gpu is not None:
+           # images = images.cuda(args.gpu, non_blocking=True)
+        #if torch.cuda.is_available():
+          #  target = target.cuda(args.gpu, non_blocking=True)
+        
+        
+        # compute loss
+        optimizer.zero_grad()
+        loss, summaries = model.loss_function(images)
 
         # measure accuracy and record loss
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
         losses.update(loss.item(), images.size(0))
-        top1.update(acc1[0], images.size(0))
-        top5.update(acc5[0], images.size(0))
+        #top1.update(acc1[0], images.size(0))
+        #top5.update(acc5[0], images.size(0))
 
         # compute gradient and do SGD step
-        optimizer.zero_grad()
+
         loss.backward()
-        optimizer.step()
+
+        with torch.no_grad():
+            optimizer.zero_grad()
+            optimizer.step()
 
         # measure elapsed time
         itr_time = time.time() - end
@@ -168,7 +182,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         metric_df.loc[len(metric_df), :] = np.array([epoch, i, dataloading_time, itr_time-dataloading_time, itr_time])
         if i % 10 == 0:
             progress.display(i)
-    metric_df.to_csv("epoch{}_iteration{}.csv".format(epoch, i), index=False)
+    # prof.export_chrome_trace("trace_epoch{}_iter{}.json".format(epoch, i))
+    metric_df.to_csv("{}_batch{}_worker{}_client{}_epoch{}_iteration{}.csv".format(args.model, args.batch, args.worker, args.client ,epoch, i), index=False)
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
@@ -176,15 +191,24 @@ def adjust_learning_rate(optimizer, epoch, args):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 # model = models.__dict__[args.arch]()
-model = models.resnet18(pretrained=False)
 
-# define loss function (criterion) and optimizer
-criterion = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(model.parameters(), args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+
+
+if args.model == "vae":
+    model = VAE()
+    model = model.to(device)
+    
+ # define  optimizer   
+
+optimizer = torch.optim.Adam(model.parameters(), lr = args.lr)
+
+
+
+
 
 for epoch in range(args.epochs):
     # if args.distributed:
@@ -193,7 +217,7 @@ for epoch in range(args.epochs):
 
     # train for one epoch
     print("epoch: {}".format(epoch))
-    train(train_loader, model, criterion, optimizer, epoch, args)
+    train(train_loader, model, optimizer, epoch, args)
 
     # evaluate on validation set
     # acc1 = validate(val_loader, model, criterion, args)
@@ -240,3 +264,16 @@ for epoch in range(args.epochs):
 #             ds.client.putObject( key, file_name)
 #         except Exception as e:
 #             print("error in putting {}: {}".format(key, e))
+
+# index = 0
+# data_folder = "/opt/ansible/examples/data/mapillary/"
+# for target, continent in enumerate(os.listdir(data_folder)):
+#     for i in range(5000):
+#         for fig in  os.listdir(os.path.join(data_folder, continent)):
+#             file_name = os.path.join(data_folder, continent, fig)
+#             key = "{}-image-{}_{}".format("map-train", index , target)
+#             try:
+#                 train_ds.client.putObject( key, file_name)
+#             except Exception as e:
+#                 print("error in putting {}: {}".format(key, e))
+#             index+=1
