6330139b92ea80b8a66db2ab597261c3143cd258
somnath.s <somnath.s@samsung.com>
2022-01-18 09:27:59 -0800

    Added new NeuralNetwork class from models.py - Updated code to train a model with train_dataloader from PyTorch 	modified:   config/config.json 	modified:   framework.py 	new file:   models.py

--- framework.py
+++ framework.py
@@ -5,9 +5,12 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D,
 
 # PyTorch library
 from torch.utils.data import DataLoader
+import torch.optim as optimization
 from dataset import DataSet, TorchImageClassificationDataset
 from datetime import datetime
 import numpy as np
+
+from models import NeuralNetwork, Net
 
 class DNNFramework(object):
     def __init__(self, config):
@@ -17,7 +20,6 @@ class DNNFramework(object):
         self.labels = []
         self.dataset = None
 
-        self.computation = config.get("computation", "CPU")
         self.framework = config.get("framework", {})
         self.framework_name = self.framework.get("name", None)
         self.categories = config["dataset"].get("label", [])
@@ -30,7 +32,13 @@ class DNNFramework(object):
         self.image_dimension = config["dataset"]["image_dimension"]
 
         # Computation
-        self.computation = config["computation"]
+        self.device = config["device"].lower() # GPU /CPU
+        if not self.device or self.device == "gpu":
+            if torch.cuda.is_available():
+                self.device = 'cuda'
+            else:
+                self.device = 'cpu'
+
 
         # Execution
         if config["execution"]["sequential"]:
@@ -136,11 +144,15 @@ class PyTorch(DNNFramework):
     def __init__(self,config):
         DNNFramework.__init__(self,config)
 
+
     def initialize(self):
         """
         Initialize the framework with creating dataset, dnn model and compiling a model.
         :return: None
         """
+        # Load few required libraries
+        #import torch.optim as optimization
+
         self.create_dataset()
         self.create_data_loader()
         #self.divide_feature_label()
@@ -160,13 +172,38 @@ class PyTorch(DNNFramework):
         )
 
         dataiter = iter(self.train_dataloader)
-        image , label = dataiter.next()
-        print("Train Data:{},{}".format(image,label))
+        train_image , train_label = dataiter.next()
+        print("Train Data:{},{}".format(train_image.size(),train_label.size()))
     def create_model(self):
-        pass
-
+        print("Device:{}".format(self.device))
+        #self.model = NeuralNetwork().to(self.device)
+        self.model = Net()
+        print(self.model)
     def training(self):
-        pass
+        criterion = self.model.loss_function()
+        optimizer = optimization.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        for epoch in range(self.framework_epochs):
+            running_loss = 0.0
+            for i, data in enumerate(self.train_dataloader, 0):
+                images, labels = data
+                # Zero the parameter gradient
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.model(images)
+                loss = criterion(outputs,labels)  # loss calculation based on CrossEntropy
+                loss.backward()
+                optimizer.step()
+
+                # Print train stats
+                running_loss += loss.item()
+                if i % 2000 ==  1999:
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+
+        print("INFO: Training is done")
+
+
 
     def training_distributed_data_parallel(self):
         """
