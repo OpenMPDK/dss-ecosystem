38a11e360b2865aaed4eadbce991ce889c25a4bc
Matthew Durand <matt.d@samsung.com>
2022-03-10 17:11:48 -0800

    move utils into load_gcc script

--- dss_client/scripts/load_gcc.sh
+++ dss_client/scripts/load_gcc.sh
@@ -33,15 +33,68 @@
 # Load GCC for DSS build an runtime environment
 set -e
 
-# Load utility functions
-. "$SCRIPT_DIR/utils.sh"
-
 # Minimum and Maximum GCC versions
 GCCMINVER=5.1.0
 GCCMAXVER=5.5.0
 
 # GCC Setenv scripts for CentOS
 GCCSETENV='/usr/local/bin/setenv-for-gcc510.sh'
+
+# Print a message to console and return non-zero
+die()
+{
+    echo "$*"
+    exit 1
+}
+
+# Compare two version strings
+vercomp () {
+    if [[ "$1" == "$2" ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    # shellcheck disable=SC2206
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+# Test two version strings
+testvercomp () {
+    vercomp "$1" "$2"
+    case $? in
+        0) op='=';;
+        1) op='>';;
+        2) op='<';;
+    esac
+    if [[ "$op" != "$3" ]]
+    then
+        return 1
+    else
+        return 0
+    fi
+}
 
 # Load GCC 5.1.0 paths if the GCCSETENV script is present
 if test -f "$GCCSETENV"
