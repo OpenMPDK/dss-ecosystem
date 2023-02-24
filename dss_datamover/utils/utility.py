226ff97150472ed1b7970602e384fc1d56872333
nsarras <nadim.sarras@gmail.com>
2023-02-24 10:55:06 -0800

    Fixing sonar cloud issue, and potential bug in get_s3_prefix function

--- dss_datamover/utils/utility.py
+++ dss_datamover/utils/utility.py
@@ -181,7 +181,7 @@ def remoteExecution(host, username, password="", cmd="", blocking=False):
         return client, stdin, stdout, stderr
 
 
-def get_s3_prefix(logger, nfs_cluster, prefix=None):
+def get_s3_prefix(logger, nfs_cluster, prefix):
     """
     Validate prefix for minio S3 and return the same.
     :param logger:
